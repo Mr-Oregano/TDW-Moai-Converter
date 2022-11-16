064329f17abf4eff31a3a77e21505dd42aad2383
Ethan Christensen <55725575+ethanlchristensen@users.noreply.github.com>
2022-11-16 01:18:27 -0600

    Drums
    Added drum notation capabilities. Rather than inputting a Note and Pitch, the user can just use the character "H" to specify default pitch and note

--- moai.py
+++ moai.py
@@ -51,14 +51,13 @@ SHELL_COMMANDS = {
     'bruh': {"args": ["", "-n"], "help-info": ["", "-n <number>"], "argc": 2},
     'what.da.hell': {"args": [""], "help-info": [""], "argc": 1},
     'what.da.dog.doin': {"args": [""], "help-info": [""], "argc": 1},
-    'dr.spice.cranberry': {"args": ["", "-n", "-d"], "help-info": ["", "-n", "-d"], "argc": 3}
+    'dr.spice.cranberry': {"args": [""], "help-info": [""], "argc": 1}
 }
 if not PS:
     SHELL_COMMANDS.pop("what.da.hell")
     SHELL_COMMANDS.pop("what.da.dog.doin")
 BASE_TAG = "BRUH >>> "
 WELCOME = ">>> WELCOME TO BRUH SHELL <<<\n\nRUN \"help\" TO SEE WHAT COMMANDS BRUH SHELL SUPPORT\n"
-
 
 def get_sounds():
     with open('sounds/sounds.txt', 'r', encoding='utf8') as sounds:
@@ -68,45 +67,26 @@ def parse_note_token(token, sound):
     if token == '.':
         return SILENCE
 
-    delim = len(token) - 1
 
-    try:
-        relative_pitch = KEYS_TO_PITCH[token[:delim]]
-        octave = int(token[delim:]) - 4 
+    elif token == 'H':
+        octave = 0
+        relative_pitch = 0
+    else:
+        delim = len(token) - 1
+        try:
+            relative_pitch = KEYS_TO_PITCH[token[:delim]]
+            octave = int(token[delim:]) - 4 
 
-    except KeyError as e:
-        print(f'Unknown note {e}. Failed to parse.', file=sys.stderr)
-        quit()
+        except KeyError as e:
+            print(f'Unknown note {e}. Failed to parse.', file=sys.stderr)
+            return -1
 
-    except ValueError:
-        print(f'Note missing pitch \'{token}\'. Failed to parse.', file=sys.stderr)
-        quit()
+        except ValueError:
+            print(f'Note missing pitch \'{token}\'. Failed to parse.', file=sys.stderr)
+            return -1
 
     pitch = relative_pitch + octave * 12
     return f"{sound}@{pitch}"
-
-def get_input_file_name():
-    it = iter(range(1, len(sys.argv)))
-
-    for i in it:
-        val = sys.argv[i]
-        if val.startswith('-'):
-            next(it) # Skip next value
-            continue
-    
-        return val 
-
-    print(f'Usage: {sys.argv[0]} <input_file> [OPTIONS]', file=sys.stderr)
-    quit()
-
-def get_output_file_name():
-    try:
-        loc = sys.argv.index('-o')
-        assert loc < len(sys.argv) - 1, 'Must provide argument for -o'
-        return sys.argv[loc + 1]
-
-    except ValueError:
-        return input('Enter output filename: ')
 
 def bruh():
     print("⡏⠉⠉⠉⠉⠉⠉⠋⠉⠉⠉⠉⠉⠉⠋⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠙⠉⠉⠉⠹\n⡇⢸⣿⡟⠛⢿⣷⠀⢸⣿⡟⠛⢿⣷⡄⢸⣿⡇⠀⢸⣿⡇⢸⣿⡇⠀⢸⣿⡇⠀\n⡇⢸⣿⣧⣤⣾⠿⠀⢸⣿⣇⣀⣸⡿⠃⢸⣿⡇⠀⢸⣿⡇⢸⣿⣇⣀⣸⣿⡇⠀\n⡇⢸⣿⡏⠉⢹⣿⡆⢸⣿⡟⠛⢻⣷⡄⢸⣿⡇⠀⢸⣿⡇⢸⣿⡏⠉⢹⣿⡇⠀\n⡇⢸⣿⣧⣤⣼⡿⠃⢸⣿⡇⠀⢸⣿⡇⠸⣿⣧⣤⣼⡿⠁⢸⣿⡇⠀⢸⣿⡇⠀\n⣇⣀⣀⣀⣀⣀⣀⣄⣀⣀⣀⣀⣀⣀⣀⣠⣀⡈⠉⣁⣀⣄⣀⣀⣀⣠⣀⣀⣀⣰")
@@ -179,10 +159,7 @@ def process_command(cmd, arg, arg_val):
         except:
             print("\t [CRINGE NOT BASED ERROR]: what.da.dog.doin encountered a non-griddy error :(")
     elif cmd == "dr.spice.cranberry":
-        if arg == "-d":
-            drspicecranberry("drum")
-        else:
-            drspicecranberry("norm")
+        drspicecranberry()
 
 def shell_bosser():
     print(WELCOME)
@@ -193,7 +170,7 @@ def shell_bosser():
             command, argument, arguement_value = tokenize_command(input(BASE_TAG))
         process_command(command, argument, arguement_value)
 
-def drspicecranberry(mode):
+def drspicecranberry():
 
     snds = get_sounds()
 
@@ -201,45 +178,47 @@ def drspicecranberry(mode):
     voices = []
     sounds = []
 
-    if mode == "norm":
-        input_file_path = "samples/" + input("\t enter a input file for parsing (from samples): ")
-        while not os.path.exists(input_file_path):
-            input_file_path = "samples/" + input("\t [CRINGE ERROR]: enter a VALID input file for parsing (from samples): ")
-        output_file_path = "results/" + input("\t enter an output file to create or write to: ")
-        with open(input_file_path, 'r') as in_file:
-            # Load Speed
-            try:
-                speed = int(in_file.readline())
-            except:
-                print("\t [CRINGE ERROR]: no speed was given. check your input file")
+    input_file_path = "samples/" + input("\t enter a input file for parsing (from samples): ")
+    while not os.path.exists(input_file_path):
+        input_file_path = "samples/" + input("\t [CRINGE ERROR]: enter a VALID input file for parsing (from samples): ")
+    output_file_path = "results/parsed_" + input_file_path[8:]
+    with open(input_file_path, 'r') as in_file:
+        # Load Speed
+        try:
+            speed = int(in_file.readline())
+        except:
+            print("\t [CRINGE ERROR]: no speed was given. check your input file")
+            return
+        # Load Notes
+        for line in in_file:
+            track = line.strip().split()
+            if not track[0] in snds:
+                print(f'\t [CRINGE ERROR]: "{track[0]}" is not a valid sound. check input file')
                 return
-            # Load Notes
-            for line in in_file:
-                track = line.strip().split()
-                if not track[0] in snds:
-                    print(f'\t [CRINGE ERROR]: "{track[0]}" is not a valid sound. check input file')
+            sounds.append(track[0])
+            voices.append(map(str.upper, track[1:]))
+
+        assert len(voices) == len(sounds), f'\t [CRINGE WARNING] sounds ({len(sounds)}) and Voices ({len(voices)}) should be equal in length'
+    print("\t parsing . . .")
+    with open(output_file_path, 'w') as out_file:
+        out_file.write(f'{SPEED_SET}{speed}|')  
+
+        for voices in zip(*voices):
+            out_file.write('|')
+            chord = []
+            for i, v in enumerate(voices):
+                check = parse_note_token(v, sounds[i])
+                if check == -1:
                     return
-                sounds.append(track[0])
-                voices.append(map(str.upper, track[1:]))
-
-            assert len(voices) == len(sounds), f'sounds ({len(sounds)}) and Voices ({len(voices)}) should be equal in length'
-        print("         parsing . . .")
-        with open(output_file_path, 'w') as out_file:
-            out_file.write(f'{SPEED_SET}{speed}|')  
-
-            for voices in zip(*voices):
-                out_file.write('|')
-                chord = [ parse_note_token(v, sounds[i]) for i, v in enumerate(voices) ]
-                chord_str = f'|{COMBINE}|'.join(chord)
-                out_file.write(chord_str)
-        print("         done!")
-    elif mode == "drum":
-        print("\t -d mode is not yet implemented. L.")
+                chord.append(check)
+            chord_str = f'|{COMBINE}|'.join(chord)
+            out_file.write(chord_str)
+    print(f"\t saving file to {output_file_path}")
+    print(f"\t done!")
 
 def main():
     bruh()
     shell_bosser()
-
 
 if __name__ == '__main__':
     main()
